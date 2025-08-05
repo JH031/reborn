e24adac8a61431e22d6fd52c8ad0168f3f3a7336
Juhyun Park <pjh33188@chungbuk.ac.kr>
2025-08-05 09:32:53 +0900

    Swagger 설정 추가

--- src/main/java/spl/reborn/config/SecurityConfig.java
+++ src/main/java/spl/reborn/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users/signup").permitAll() // 회원가입은 누구나 접근 가능
+                        .requestMatchers("/api/users/signup","/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() // 회원가입은 누구나 접근 가능
                         .anyRequest().authenticated() // 나머지는 인증 필요
                 );
 
