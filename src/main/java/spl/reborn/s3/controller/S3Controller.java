cedae2467eb18373b3e030ec256e4841ad86a19e
Juhyun Park <pjh33188@chungbuk.ac.kr>
2025-08-09 21:01:36 +0900

    feat: S3 이미지 등록 구현

--- src/main/java/spl/reborn/s3/controller/S3Controller.java
+++ src/main/java/spl/reborn/s3/controller/S3Controller.java
@@ -1,6 +1,7 @@
 package spl.reborn.s3.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,14 +10,13 @@ import spl.reborn.s3.service.S3Service;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/upload")
 @RequiredArgsConstructor
 public class S3Controller {
     private final S3Service s3Service;
 
-    @PostMapping
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) throws IOException {
         String url = s3Service.uploadFile(file);
-        return ResponseEntity.ok(url); // 200 OK + body
+        return ResponseEntity.ok(url);
     }
 }
--- src/main/java/spl/reborn/s3/service/S3Service.java
+++ src/main/java/spl/reborn/s3/service/S3Service.java
@@ -1,14 +1,20 @@
 package spl.reborn.s3.service;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -20,27 +26,46 @@ public class S3Service {
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
 
-    private String defaultUrl = "https://s3.amazonaws.com/";
-
+    /** 업로드하고 퍼블릭 URL 반환 */
     public String uploadFile(MultipartFile file) throws IOException {
-        String fileName = generateFileName(file);
-        try{
-            s3Client.putObject(bucketName,fileName,file.getInputStream(),getObjectMetadata(file));
-            return defaultUrl+fileName;
-        }catch(SdkClientException e){
-            throw new IOException("Error uploading file to S3",e);
+        String objectKey = generateFileName(file);
+
+        try {
+            ObjectMetadata metadata = buildMetadata(file);
+
+            // 퍼블릭으로 바로 접근 가능하게 업로드 (버킷이 퍼블릭 차단이면 presigned URL 사용 필요)
+            PutObjectRequest req = new PutObjectRequest(
+                    bucketName, objectKey, file.getInputStream(), metadata
+            ).withCannedAcl(CannedAccessControlList.PublicRead);
+
+            s3Client.putObject(req);
+
+            String region = s3Client.getRegionName(); // 예: ap-northeast-2
+            String encodedKey = URLEncoder.encode(objectKey, StandardCharsets.UTF_8);
+
+            // 권장 virtual-hosted-style URL
+            return String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, encodedKey);
+
+        } catch (AmazonServiceException e) {
+            // S3가 에러 응답을 보낸 경우
+            throw new IOException("S3 error: " + e.getErrorMessage(), e);
+        } catch (SdkClientException e) {
+            // 네트워크 등 클라이언트 측 문제
+            throw new IOException("Client error when uploading to S3", e);
         }
     }
 
-    private ObjectMetadata getObjectMetadata(MultipartFile file) {
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentType(file.getContentType());
-        objectMetadata.setContentLength(file.getSize());
-        return objectMetadata;
+    private ObjectMetadata buildMetadata(MultipartFile file) {
+        ObjectMetadata meta = new ObjectMetadata();
+        String contentType = file.getContentType();
+        meta.setContentType(contentType != null ? contentType : "application/octet-stream");
+        meta.setContentLength(file.getSize());
+        return meta;
     }
 
     private String generateFileName(MultipartFile file) {
-        return UUID.randomUUID().toString()+"-"+file.getOriginalFilename();
+        String original = Objects.requireNonNullElse(file.getOriginalFilename(), "file");
+        // 원본 파일명 그대로 쓰고 URL에서만 인코딩 (S3 키는 인코딩 불필요)
+        return UUID.randomUUID() + "-" + original;
     }
-
 }
