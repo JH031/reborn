948bea9b44e870ce052006c96e97ccd455545272
shinhyew <she8729me@naver.com>
2025-08-16 23:59:46 +0900

    비밀번호 재설정 오류

--- src/main/java/spl/reborn/notification/PwEmailServiceImpl.java
+++ src/main/java/spl/reborn/notification/PwEmailServiceImpl.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @Service
 @RequiredArgsConstructor
@@ -11,9 +13,13 @@ public class PwEmailServiceImpl implements PwEmailService {
 
     private final JavaMailSender mailSender;
 
+    @Value("${app.mail.from}")
+    private String from;
+
     @Override
     public void sendText(String toEmail, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);          // ★ 핵심: 반드시 지정
         message.setTo(toEmail);
         message.setSubject(subject);
         message.setText(body);
