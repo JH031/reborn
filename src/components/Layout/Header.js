5b833372e194fa350eacf0f614d0a51d0d4bf2a3
taejung77 <132897696+taejung77@users.noreply.github.com>
2025-08-20 19:50:49 +0900

    헤더 로그아웃 빼기

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,30 +1,30 @@
-// src/components/Layout/Header.js
-
 import React from 'react';
 import { useAuth } from '../../context/AuthContext';
 import './Header.css';
 
-const Header = ({ onLoginClick, onMenuClick }) => { // onMenuClick prop 추가
-    const { user, logout } = useAuth();
+const Header = ({ onLoginClick, onMenuClick }) => {
+  const { user } = useAuth(); // ✅ logout 제거 (경고 해결)
 
-    return (
-        <div className="header">
-            <div className="header-left">
-                <div className="menu-icon" onClick={onMenuClick}>☰</div>
-                <div className="logo">REBORN</div>
-            </div>
-            <div className="header-right">
-                {user ? (
-                    <div className="user-info">
-                        <span>{user.name}님 환영합니다!</span>
-                        <button className="logout-btn" onClick={logout}>로그아웃</button>
-                    </div>
-                ) : (
-                    <button className="login-btn" onClick={onLoginClick}>로그인</button>
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div className="header">
+      <div className="header-left">
+        <div className="menu-icon" onClick={onMenuClick}>☰</div>
+        <div className="logo">REBORN</div>
+      </div>
+      <div className="header-right">
+        {!user && (
+          <button className="login-btn" type="button" onClick={onLoginClick}>
+            로그인
+          </button>
+        )}
+        {user && (
+          <div className="user-info">
+            <span>{user.name}님 환영합니다!</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
